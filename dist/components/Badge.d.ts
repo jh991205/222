4f224cc528d779957784c03ca1f44dcf0fd90d26
Jiadi <jiadi.huang@aifocal.ai>
2023-10-31 15:30:02 -0400

    build

--- dist/components/Badge.d.ts
+++ dist/components/Badge.d.ts
@@ -1,8 +1,1 @@
-type Props = {
-    botContainer: HTMLDivElement | undefined;
-    poweredByTextColor?: string;
-    badgeBackgroundColor?: string;
-};
-export declare const Badge: (props: Props) => import("solid-js").JSX.Element;
-export {};
 //# sourceMappingURL=Badge.d.ts.map
